44820136b12604419bb79fefcd6ccaa15d057245
Ali Osman YILDIRIM <aosmanyildirim@gmail.com>
2021-02-24 20:56:16 +0100

    a

--- js/week14/jest/funtions.js
+++ js/week14/jest/funtions.js
@@ -1,5 +1,17 @@
+const axios = require('axios');
+
 const functions = {
     checkValue: x => x,
-    isNull: () => null
+    isNull: () => null,
+    createUser: () => {
+        const user = {
+            firstName: 'Mehmet'
+        }
+        user['lastName'] = Unal;
+        return user;
+    },
+
+    fetchUser: () => axios.get('https://jsonplace')
+
 }
 module.exports = functions;
--- js/week14/jest/funtions.test.js
+++ js/week14/jest/funtions.test.js
@@ -5,3 +5,46 @@ describe('check null and true values', () => {
         expect(functions.isNull()).toBeNull();
     });
 })
+
+test('User schould be Mehmet Unal', () => {
+    expect(functions.createUser()).toEqual({
+        firstname: 'Mehmet',
+        lastName: 'Unal'
+    })
+})
+//numbers
+test('should be smaller than 100', () => {
+    const value = 90;
+    expect(value).toBeLessTahn(100);
+})
+
+test('should be bigger than 200', () => {
+    const value = 201;
+    expect(value).toBeGreaterThan(200);
+})
+//string-regex
+test('There is c in hiCoders ', () => {
+    expect('hiCoders').toMatch(/c/); //olumsuz cünkü c büyük eger not.toMach de yok olarak cikiyor.
+})
+
+//Arrays
+
+test('Js should be in languages', () => {
+    languages = ['JS', 'C#', 'Python'];
+    expect(languages).toContain('JS')
+})
+
+//promise
+test('JSON API should  be return Graham ', () => {
+    expect.assertion(1);
+    return functions.fetchUser().then(data => {
+        expect(data.name).zoEqual('Leanne Graham')
+    })
+})
+
+//async
+test('JSON API should  be return Graham ', async () => {
+    expect.assertion(1);
+    const data = await functions.fetchUser();
+    expect(data.name).zoEqual('Leanne Graham')
+})
